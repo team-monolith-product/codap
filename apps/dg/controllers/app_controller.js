439c2bb8e3bd9fdc29a7d3a8dce0e6312da9185c
shcshcshc <shc@team-mono.com>
2026-05-20 13:51:53 +0900

    refactor(codap): handleRead의 unreachable IMAGE 분기 제거 [AI]
    IMAGE import가 외부 서버 업로드 경로로 분리되면서 FileReader/handleRead를
    더 이상 거치지 않으므로, handleRead 내 IMAGE 분기는 도달 불가능한 죽은
    코드가 되었다. 제거하여 handleRead가 JSON/GEOJSON/TEXT 전용임을 명확히 한다.

--- apps/dg/controllers/app_controller.js
+++ apps/dg/controllers/app_controller.js
@@ -981,8 +981,6 @@ DG.appController = SC.Object.create((function() // closure
               } else if(iType === 'TEXT') {
                 that.importText(this.result,
                    iFile.name.replace(/\.[^.]*$/, ''), iFile.name);
-              } else if(iType === 'IMAGE') {
-                that.importImage(this.result, iFile.name);
               }
               if(iDialog)
                 iDialog.close();
@@ -1000,7 +998,7 @@ DG.appController = SC.Object.create((function() // closure
           if(!iFile) return;
           // AIDEV-NOTE:
           // IMAGE 타입은 FileReader.readAsDataURL로 base64 data URL을 만드는 대신, 외부 서버에
-          // 업로드하여 URL을 받는다. data URL 임베드는 문서 직렬화 산출물을 비대화시켜 save timeout 초과를 유발한다.  
+          // 업로드하여 URL을 받는다. data URL 임베드는 문서 직렬화 산출물을 비대화시켜 save timeout 초과를 유발한다.
           if(iType === 'IMAGE') {
             try {
               var url = await DG.cfmClient.uploadBinary(iFile);
